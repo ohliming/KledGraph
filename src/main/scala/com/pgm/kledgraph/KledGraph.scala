a02e04460bbfc5af9297822664e90bf58d063bbc
ohliming <lm30go@163.com>
2017-05-26 17:07:59 +0800

    update

--- src/main/scala/com/pgm/kledgraph/KledGraph.scala
+++ src/main/scala/com/pgm/kledgraph/KledGraph.scala
@@ -100,7 +100,7 @@ object KledGraph {
           } else if (result.equals("NULL") || result.equals("")) {
             res = 2
           } else {
-            val formatRes = "\"\""+result+"\"\""
+            val formatRes = "\"\"\""+result+"\"\"\""
             val t = parse(formatRes, false)
             val mapJson = t.values.asInstanceOf[Map[String,String]]
             res =  if(mapJson("result") == "1") 1 else 2
