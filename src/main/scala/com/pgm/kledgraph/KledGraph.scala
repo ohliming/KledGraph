4106dffd721c76ca11be1c84d28bf504bb0913c2
ohliming <lm30go@163.com>
2017-06-08 18:56:39 +0800

    update

--- src/main/scala/com/pgm/kledgraph/KledGraph.scala
+++ src/main/scala/com/pgm/kledgraph/KledGraph.scala
@@ -435,8 +435,9 @@ object KledGraph {
   def sumPositionsPro(cpds:Seq[Double], posMap:Map[Int,Int], len:Int) = {
     var p = 0.0; var count = 0
     val loop = new Breaks
+    println("the len is:"+cpds.length)
     cpds.foreach(pr1=>{
-      val index = pos2Seq(count,len)
+      val index = pos2Seq(count, len)
       var bFlag = true
       loop.breakable {
         posMap.foreach(pair => {
